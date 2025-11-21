76f970adc2d7422e82cb8080e97d83cc93b68a66
QuinAustin <QuinlAustin@gmail.com>
2025-11-21 00:18:17 -0600

    adding test.js script

---
+++ test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Test script using github
+// @namespace   Violentmonkey Scripts
+// @match        *://*/*
+// @grant       none
+// @version     1.0.1
+// @author      -
+// @description This is a script to test updates through github
+//
+// @updateURL   https://github.com/QuinAustin/Userscripts/test.js
+// @downloadURL https://github.com/QuinAustin/Userscripts/test.js
+//
+//
+// ==/UserScript==
+(function() {
+    'use strict';
+    console.log("test script is running");
+})();
