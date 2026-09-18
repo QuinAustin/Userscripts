017de21549cf8119826ad4820be0a53098fefb2a
QuinAustin <QuinlAustin@gmail.com>
2026-09-18 21:55:32 +0000

    fixed a bug where navigating to subdomains would not reactivate the script

--- paragraphStringReplacer.js
+++ paragraphStringReplacer.js
@@ -1,35 +1,46 @@
 // ==UserScript==
 // @name        Paragraph String Replacer
 // @namespace   Violentmonkey Scripts
-// @version     1.0.0
-//
+// @version     1.0.3
+// @description Used for replacing words with different words
+// @author      -
 // @match       https://example.com/*
 // @grant       none
-//
-// @author      -
-// @description replace words and phrases in paragraphs, with corrections and replacements. 
+
+// @description replace words and phrases in paragraphs, with corrections and replacements.
 // ==/UserScript==
 
 
 'use strict';
-function tryReplacing(paragraph, text, findString, replaceString) {
-     if (text.includes(findString)) {
-        paragraph.textContent = text.replace(findString, replaceString);
+
+//input:
+//[1] Element with textContent
+//[2] the result of [1].textContent()
+//[3] Words in list to replace
+//[4] Replacement word to be used for .replace() [3]
+
+//result:
+//the textContent of [1] has all instances of [3] replaced with [4]
+function replaceText(textElement, text, wordList, replacementWord) {
+  wordList.forEach(word => {
+    if (text.includes(word)) {
+      textElement.textContent = text.replace(word, replacementWord);
     }
+  });
 }
 
 function changeSpellings() {
-    console.debug("checking paragraphs");
-    const paragraphs = document.querySelectorAll(".overflow-visible p"); //use the parent element of 'p' here as well.
-    paragraphs.forEach(paragraph => {
-        let text = paragraph.textContent
-        tryReplacing(paragraph, text, "mispeling", "misspelling") //call function with the word to be replaced, and the word it is to be replaced with.
-        tryReplacing(paragraph, text, "foo", "bar") //consecutive functions can use the same text variable for memoisation purposes.
-    })
+    console.debug("Checking Text Element");
+    //could use a switch case to determine what element or word list to use, based on the document url.
+    const textElements = document.querySelectorAll(".overflow-visible p"); //Parent Element of Text Body
+    textElements.forEach(textEl => {
+        let text = textEl.textContent;
+        replaceText(textEl, text, ["foo", "bar"], ("baz"));
+      });
 }
 
 function paragraphsLoaded() {
-    let content = document.querySelector(".overflow-visible p"); //replace with parent element of "p" elements, so they have time to load.  it also helps narrow down the scope of the query.
+    let content = document.querySelector(".overflow-visible p"); //Parent Element of Text Body
     if (content) {
         return true;
     }
@@ -38,14 +49,9 @@ function paragraphsLoaded() {
 function observeQuery(query, funCondition, funFinish) {
     const observer = new MutationObserver(() => {
       if (funCondition()) {
-        observer.disconnect();
-        console.debug("Query Condition Met");
         funFinish();
-        return true;
       }
     });
     observer.observe(query, { childList: true, subtree: true });
 }
-
-console.log("script started");
 observeQuery(document.body, paragraphsLoaded, changeSpellings);
